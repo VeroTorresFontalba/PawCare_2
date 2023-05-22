587d6bd1868ff65ea84cfef862fb2c2ba4e85000
Felipe <f.garcia@duocuc.cl>
2023-05-22 14:55:54 -0400

    Guarda cambio

--- PawCare-1/applications/users/urls.py
+++ PawCare-1/applications/users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 app_name ="users_app"
 
 
@@ -10,4 +12,8 @@ urlpatterns = [
     path('registro/', views.UserRegisterView.as_view(),name='user_register'),
     path('login/', views.LoginUser.as_view(),name='user_login'),
     path('logout/', views.LogoutView.as_view(),name='user_logout'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_send/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
